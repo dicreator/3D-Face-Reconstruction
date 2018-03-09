3b980acf2e8225ee5a0bc3520a799ed2370b0634
Dinmukhamed <sc15dk@leeds.ac.uk>
2018-03-09 10:26:59 +0000

    only 1 face

--- 1p.cpp
+++ 1p.cpp
@@ -29,42 +29,49 @@ int main( int argc, char** argv){
   int h = 600;
 
   //for boundary boxes
+  //function returns object_detector
   frontal_face_detector detector = get_frontal_face_detector();
   //predict face landmark positions - 68 .dat import
   shape_predictor fshape;
-  deserialize(argv[1]) >> fshape; // change two lines into one
+
+  deserialize("shape_predictor_68_face_landmarks.dat") >> fshape; // change two lines into one
 
   image_window win, win_faces;
   array2d<rgb_pixel> img;
   //
-load_image(img,argv[2]);
+load_image(img,argv[1]);
 
 //increase image?
 pyramid_up(img);
 
 std::vector<dlib::rectangle> dets = detector(img);
 
-cout << "Number of faces detected: " << dets.size() << endl;
+//if more than one face was detected
+if (dets.size() > 1) {
+  cout << "More than one face was detected" << endl;
+  cout << "We will proceed with the first detected face" << endl;
+}
+
 
 // std::std::vector<full_object_detection> shapes; //for each of faces can be removed
-std::vector<full_object_detection> shape;
+// std::vector<full_object_detection> shape;
 full_object_detection ws = fshape(img,dets[0]);
 cout << "number of parts: "<< ws.num_parts() << endl;
-cout << "pixel position of first part:  " << ws.part(0) << endl;
+cout << "pixel position of first part:  " << ws.part(0)[0] << endl;
 cout << "pixel position of second part: " << ws.part(67) << endl;
-shape.push_back(ws);
+// shape.push_back(ws);
 
 
 //render face to a window
 win.clear_overlay();
 win.set_image(img);
 //show face landmark
-win.add_overlay(render_face_detections(shape));
+win.add_overlay(render_face_detections(ws));
 
 //crop face *don't need for now
-dlib::array<array2d<rgb_pixel>> face_chip;
-extract_image_chips(img, get_face_chip_details(shape), face_chip);
-win_faces.set_image(tile_images(face_chip));
+dlib::array2d<rgb_pixel> face_chip;
+extract_image_chip(img, get_face_chip_details(ws), face_chip);
+win_faces.set_image(face_chip);
 
  cin.get();
 //wait for a character to exit
